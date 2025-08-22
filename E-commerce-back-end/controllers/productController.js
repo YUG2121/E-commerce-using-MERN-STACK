64935cd63b0bb7ee5fc42d5f7a51a7e8209732bd
Yug-upadhyay <yugnum1@gmail.com>
2025-08-22 23:31:42 +0530

    BackEnd-of-ecommerce-web-app

---
+++ E-commerce-back-end/controllers/productController.js
@@ -0,0 +1,67 @@
+import { v2 as cloudinary } from "cloudinary";
+import { json } from "express";
+import productModel from "../models/productmodel.js";
+
+//function for add product
+const addProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      SubCategory,
+      sizes,
+      BestSeller,
+    } = req.body;
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
+
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined
+    );
+    let imageUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: 'image',
+        });
+        return result.secure_url;
+      })
+    );
+ const productData = {
+    name,
+    description,
+    category,
+    price:Number(price),
+    SubCategory,
+    BestSeller: BestSeller === "true"? true : false,
+    sizes : JSON.parse(sizes),
+    image: imageUrl,
+    date: Date.now()
+ }
+
+console.log(productData);
+const product = new productModel(productData)
+await product.save()
+
+
+    res.json({success:true,message:'product added'});
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+    console.log(error);
+  }
+};
+
+//function for list product
+const listProduct = (req, res) => {};
+//function for remove product
+const removeProduct = (req, res) => {};
+//function for single product info
+const singleProduct = (req, res) => {};
+
+export { addProduct, listProduct, removeProduct, singleProduct };
